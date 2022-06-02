124ab136e1fbe7e9611d6f39a2352ce84d02f8c8
Prashanth14 <kadireprashanthreddy1@gmail.com>
2022-06-01 20:37:13 -0400

    Initial commit

---
+++ CountPairsWithGivenSum.cpp
@@ -0,0 +1,26 @@
+// Brute force Method with O(n^2), which is not efficent solution
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int arr[] = {1, 1, 1, 1};
+    
+//     int m = sizeof(arr) / sizeof(arr[0]);
+//     int num_pairs = 0, sum = 2;
+    
+//     for(int i = 0; i < m; i++){
+//         for(int j = i+1; j < m; j++){
+//             if(arr[i] + arr[j] == sum){
+//                 num_pairs++;
+//             }
+//         }
+//     }
+//     cout<<"The number of pairs = "<<num_pairs<<endl;
+
+    
+//     return 0;
+// }
+
+
