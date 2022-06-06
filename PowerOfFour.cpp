0fab6312a9e870f34ede8971cdc3cd02c2abc44b
Prashanth14 <kadireprashanthreddy1@gmail.com>
2022-06-05 22:00:31 -0400

    Initial commit

---
+++ PowerOfFour.cpp
@@ -0,0 +1,39 @@
+// Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+// An integer n is a power of four, if there exists an integer x such that n == 4x.
+// Example 1:
+
+// Input: n = 16
+// Output: true
+// Example 2:
+
+// Input: n = 5
+// Output: false
+// Example 3:
+
+// Input: n = 1
+// Output: true
+
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n <= 0 || n == 2 || n == 3)
+            return false;
+        
+        if(n == 1 || n == 4)
+            return true;
+        
+        
+        while(n){
+            if(n % 4 == 0){
+                n = n / 4;
+                if(n == 1)
+                return true;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
