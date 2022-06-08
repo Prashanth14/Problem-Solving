477962b7b97598e69f611e540f310c1c76e023f7
Prashanth14 <kadireprashanthreddy1@gmail.com>
2022-06-08 18:00:46 -0400

    Initial commit

---
+++ XOROperationInArray.cpp
@@ -0,0 +1,34 @@
+// You are given an integer n and an integer start.
+
+// Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
+
+// Return the bitwise XOR of all elements of nums.
+
+// Example 1:
+
+// Input: n = 5, start = 0
+// Output: 8
+// Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+// Where "^" corresponds to bitwise XOR operator.
+// Example 2:
+
+// Input: n = 4, start = 3
+// Output: 8
+// Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8.
+
+
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        vector<int> nums;
+        for(int i = 0; i< n; i++){
+            int x = start + 2 * i;
+            nums.push_back(x);
+        }
+        int output = 0;
+        for(int i = 0; i< nums.size(); i++){
+            output = output ^ nums[i];
+        }
+        return output;
+    }
+};
