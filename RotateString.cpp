0a90b49b22a03a0a8540d27c2d071f1af168570d
Prashanth14 <kadireprashanthreddy1@gmail.com>
2022-06-12 19:23:30 -0400

    Initial commit

---
+++ RotateString.cpp
@@ -0,0 +1,43 @@
+// Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+// A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+// For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+// Example 1:
+
+// Input: s = "abcde", goal = "cdeab"
+// Output: true
+// Example 2:
+
+// Input: s = "abcde", goal = "abced"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= s.length, goal.length <= 100
+// s and goal consist of lowercase English letters.
+
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+
+        int i = 0;
+        while(i < s.size()){
+            char t = s[0];
+            int j = 0;
+            while(j < s.size()-1){
+                s[j] = s[j + 1];
+                j++;
+            }
+            s[s.size()-1] = t;
+            if(s == goal)
+                return true;
+            i++;
+        }
+        return false;
+    }
+};
