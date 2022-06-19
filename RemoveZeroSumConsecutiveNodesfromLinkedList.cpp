a977133277cae419e02de6be904893b6bd7f1e52
Prashanth14 <kadireprashanthreddy1@gmail.com>
2022-06-18 22:03:23 -0400

    Initial commit

---
+++ RemoveZeroSumConsecutiveNodesfromLinkedList.cpp
@@ -0,0 +1,71 @@
+// Given the head of a linked list, we repeatedly delete consecutive sequences of nodes that sum to 0 until there are no such sequences.
+
+// After doing so, return the head of the final linked list.  You may return any such answer.
+
+ 
+
+// (Note that in the examples below, all sequences are serializations of ListNode objects.)
+
+// Example 1:
+
+// Input: head = [1,2,-3,3,1]
+// Output: [3,1]
+// Note: The answer [1,2,1] would also be accepted.
+// Example 2:
+
+// Input: head = [1,2,3,-3,4]
+// Output: [1,2,4]
+// Example 3:
+
+// Input: head = [1,2,3,-3,-2]
+// Output: [1]
+ 
+
+// Constraints:
+
+// The given linked list will contain between 1 and 1000 nodes.
+// Each node in the linked list has -1000 <= node.val <= 1000.
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        if(head == NULL)
+            return NULL;
+            
+        unordered_map<int, ListNode*> m;
+        ListNode *dummy = new ListNode(0);
+        dummy -> next = head;
+        m[0] = dummy;
+        int prefixsum = 0;
+        while(head){
+            prefixsum += head -> val;
+            if( m.find(prefixsum) != m.end() ){
+                ListNode *start = m[prefixsum];
+                int sum  =  prefixsum;
+                while(start != NULL && start != head){
+                    start = start -> next;
+                    sum += start -> val;
+                    if(start != head)
+                        m.erase(sum);
+                }
+                m[prefixsum] -> next = head -> next;
+            }else{
+                m[prefixsum] = head;
+            }
+            head = head -> next;
+        }
+        return dummy -> next;
+    }
+};
+
