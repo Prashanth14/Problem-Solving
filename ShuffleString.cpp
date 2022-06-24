fc2a1335c5e67fd1a2ed0dd0facc31abe8d0d58d
Prashanth14 <kadireprashanthreddy1@gmail.com>
2022-06-23 20:49:51 -0400

    initial commit

---
+++ ShuffleString.cpp
@@ -0,0 +1,43 @@
+// You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+// Return the shuffled string.
+
+// Example 1:
+
+
+// Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+// Output: "leetcode"
+// Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+// Example 2:
+
+// Input: s = "abc", indices = [0,1,2]
+// Output: "abc"
+// Explanation: After shuffling, each character remains in its position.
+ 
+
+// Constraints:
+
+// s.length == indices.length == n
+// 1 <= n <= 100
+// s consists of only lowercase English letters.
+// 0 <= indices[i] < n
+// All values of indices are unique.
+
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        map<int,char> m;
+        int n = indices.size();
+        
+        for(int i = 0; i < n; ++i){
+            m[indices[i]] = s[i];
+        }
+        
+        string shuffle_str = "";
+        for(int i = 0; i < n; ++i)
+            shuffle_str += m[i];
+        
+        return shuffle_str;
+    }
+};
