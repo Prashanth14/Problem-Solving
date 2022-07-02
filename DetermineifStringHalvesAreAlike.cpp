293125eded7e50ce718fd9162cc1036a63de4807
Prashanth14 <kadireprashanthreddy1@gmail.com>
2022-07-02 19:12:40 -0400

    initial commit

---
+++ DetermineifStringHalvesAreAlike.cpp
@@ -0,0 +1,44 @@
+// You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+
+// Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+
+// Return true if a and b are alike. Otherwise, return false.
+
+// Example 1:
+
+// Input: s = "book"
+// Output: true
+// Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+// Example 2:
+
+// Input: s = "textbook"
+// Output: false
+// Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+// Notice that the vowel o is counted twice.
+ 
+
+// Constraints:
+
+// 2 <= s.length <= 1000
+// s.length is even.
+// s consists of uppercase and lowercase letters.
+
+
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int n = s.length();
+        int i = 0, j = n / 2;
+        int c1 = 0, c2 = 0;
+        for(int i = 0; i < n / 2; ++i){
+            if(s[i] == 'a' ||  s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+                ++c1;
+        }
+        
+        for(int i = j; i < n ; ++i){
+            if(s[i] == 'a' ||  s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+                ++c2;
+        }
+        return c1 == c2? true : false;
+    }
+};
