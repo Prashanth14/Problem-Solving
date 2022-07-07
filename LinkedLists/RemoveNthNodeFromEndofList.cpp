38151c6f84da62e5c0fa422b9b4ac3b6077c711f
Prashanth14 <kadireprashanthreddy1@gmail.com>
2022-07-07 17:37:47 -0400

    initial commit

---
+++ LinkedLists/RemoveNthNodeFromEndofList.cpp
@@ -0,0 +1,55 @@
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+// Example 2:
+
+// Input: head = [1], n = 1
+// Output: []
+// Example 3:
+
+// Input: head = [1,2], n = 1
+// Output: [1]
+ 
+
+// Constraints:
+
+// The number of nodes in the list is sz.
+// 1 <= sz <= 30
+// 0 <= Node.val <= 100
+// 1 <= n <= sz
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int c = 0, nodes = 0;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        if(!head)
+            return head;
+        ++nodes;
+        removeNthFromEnd(head -> next,n);
+        ++c;
+        if(nodes == n){
+            head = head -> next;
+        }
+        if(c -1 == n){
+            ListNode* deletingNode = head -> next;
+            head -> next = deletingNode -> next;
+        }
+        return head;
+    }
+};
