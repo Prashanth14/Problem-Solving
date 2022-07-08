e2ac3d2048c3115c7107d7cb43227720d1e4ec9d
Prashanth14 <kadireprashanthreddy1@gmail.com>
2022-07-08 19:02:29 -0400

    initial commit

---
+++ LinkedLists/LinkedListRandomNode.cpp
@@ -0,0 +1,82 @@
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Implement the Solution class:
+
+Solution(ListNode head) Initializes the object with the head of the singly-linked list head.
+int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be chosen.
+ 
+
+Example 1:
+
+
+Input
+["Solution", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom"]
+[[[1, 2, 3]], [], [], [], [], []]
+Output
+[null, 1, 3, 2, 2, 3]
+
+Explanation
+Solution solution = new Solution([1, 2, 3]);
+solution.getRandom(); // return 1
+solution.getRandom(); // return 3
+solution.getRandom(); // return 2
+solution.getRandom(); // return 2
+solution.getRandom(); // return 3
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+ 
+
+Constraints:
+
+The number of nodes in the linked list will be in the range [1, 104].
+-104 <= Node.val <= 104
+At most 104 calls will be made to getRandom.
+ 
+
+ /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *temp;
+    Solution(ListNode* head) {
+        temp = head;
+    }
+    
+    int getRandom() {
+        ListNode *temp1 = temp;
+        int size = 0;
+        while(temp1){
+            ++size;
+            temp1 = temp1 -> next;
+        }
+        
+        int randnode = rand() % size + 1;
+        
+        if(randnode == 1){
+            return temp -> val;
+        }
+        
+        ListNode *temp2 = temp;
+        int i = 1;
+        while(i < randnode){
+            if(!temp2)
+                return 0;
+            temp2 = temp2 -> next;
+            ++i;
+        }
+        return temp2 -> val;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
