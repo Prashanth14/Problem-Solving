ee7f6c3d1dbf132f4139a61f24d9636e4d174827
Prashanth14 <kadireprashanthreddy1@gmail.com>
2022-07-12 19:46:57 -0400

    initial commit

---
+++ Hash Maps/SortCharactersByFrequency.cpp
@@ -0,0 +1,54 @@
+// Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+// Return the sorted string. If there are multiple answers, return any of them.
+
+// Example 1:
+
+// Input: s = "tree"
+// Output: "eert"
+// Explanation: 'e' appears twice while 'r' and 't' both appear once.
+// So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+// Example 2:
+
+// Input: s = "cccaaa"
+// Output: "aaaccc"
+// Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+// Note that "cacaca" is incorrect, as the same characters must be together.
+// Example 3:
+
+// Input: s = "Aabb"
+// Output: "bbAa"
+// Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+// Note that 'A' and 'a' are treated as two different characters.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 5 * 105
+// s consists of uppercase and lowercase English letters and digits.
+
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        map<char, int> mp;
+        int n = s.length();
+        for(int i = 0; i < n; ++i){
+            mp[s[i]]++;
+        }
+        
+        set<pair<int,char>> st;
+        for(auto m:mp){
+            st.insert({-m.second, m.first});
+        }
+        
+        string ans;
+        for(auto t: st){
+            while(t.first < 0){
+                ans += t.second;
+                t.first++;
+            }
+        }
+        return ans;
+    }
+};
